90ae69f8a2dd943f8d529515950675e03702125a
Venkat Hemant Akurati <70124609+hemantakurati@users.noreply.github.com>
2020-10-24 22:15:32 -0500

    Delete K- Nearest Neighbor.R

--- K- Nearest Neighbor.R
+++
@@ -1,9 +0,0 @@
-library(class)
-pred_knn = knn(train = training_set[, -9],
-               test = test_set[, -9],
-               cl = training_set[, 9],
-               k = 5)
-pred_knn
-
-cm_knn = table(test_set[, 9], pred_knn)
-cm_knn

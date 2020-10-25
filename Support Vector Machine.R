fe02692256cbff0e35c8eaaf6dc1cf9cd4a22629
Venkat Hemant Akurati <70124609+hemantakurati@users.noreply.github.com>
2020-10-24 22:16:01 -0500

    Delete Support Vector Machine.R

--- Support Vector Machine.R
+++
@@ -1,9 +0,0 @@
-library(e1071)
-SVM = svm(formula = Diagnosis ~ .,
-          data = training_set,
-          type = 'C-classification',
-          kernel = 'linear')
-pred_svm = predict(SVM, newdata = test_set[-9])
-pred_svm
-cm_svm = table(test_set[, 9], pred_svm)
-cm_svm
